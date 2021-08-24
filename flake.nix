1b3f321c77705e69c972b6cbc8cd24a7b9f74cb5
raghuram <raghurammailbox@gmail.com>
2021-08-24 13:19:55 +0530

    flake made with mach-nix

--- flake.nix
+++ flake.nix
@@ -1,120 +1,35 @@
 {
-  description = "(insert short project description here)";
+  description = "Deltabot : Bots manager for deltachat";
 
-  # Nixpkgs / NixOS version to use.
-  inputs.nixpkgs.url = "nixpkgs/nixos-20.09";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    mach-nix.url = "github:DavHau/mach-nix?ref=3.3.0";
+    deltabot-scr = {url = "https://files.pythonhosted.org/packages/c6/d7/90c24bbeef1e6b7e73dfe30e51e96699f5db05984497831ae2d87e93d9d4/deltabot-0.8.0.tar.gz"; flake = false;};
+  };
 
-  # Upstream source tree(s).
-  inputs.hello-src = { url = git+https://git.savannah.gnu.org/git/hello.git; flake = false; };
-  inputs.gnulib-src = { url = git+https://git.savannah.gnu.org/git/gnulib.git; flake = false; };
-
-  outputs = { self, nixpkgs, hello-src, gnulib-src }:
-    let
-
-      # Generate a user-friendly version numer.
-      version = builtins.substring 0 8 hello-src.lastModifiedDate;
-
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" ];
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-
-    in
-
-    {
-
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
-
-        hello = with final; stdenv.mkDerivation rec {
-          name = "hello-${version}";
-
-          src = hello-src;
-
-          buildInputs = [ autoconf automake gettext gnulib perl gperf texinfo help2man ];
-
-          preConfigure = ''
-            mkdir -p .git # force BUILD_FROM_GIT
-            ./bootstrap --gnulib-srcdir=${gnulib-src} --no-git --skip-po
-          '';
-
-          meta = {
-            homepage = "https://www.gnu.org/software/hello/";
-            description = "A program to show a familiar, friendly greeting";
-          };
+  outputs = { self, nixpkgs, flake-utils, mach-nix,deltabot-scr }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        python = "python38";
+        pkgs = nixpkgs.legacyPackages.${system};
+        mach-nix-wrapper = import mach-nix { inherit pkgs python; };
+        requirements     = ["setuptools" "attrs" "deltachat"];
+        pythonBuild      = mach-nix-wrapper.mkPython { inherit requirements; };
+        deltabot         = mach-nix-wrapper.buildPythonPackage rec{
+          version = "0.8.0";
+          src = deltabot-scr;
+          requirementExtra = requirements;
         };
-
-      };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) hello;
-        });
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.hello);
-
-      # A NixOS module, if applicable (e.g. if the package provides a system service).
-      nixosModules.hello =
-        { pkgs, ... }:
-        {
-          nixpkgs.overlays = [ self.overlay ];
-
-          environment.systemPackages = [ pkgs.hello ];
-
-          #systemd.services = { ... };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+            pythonBuild
+          ];
         };
-
-      # Tests run by 'nix flake check' and by Hydra.
-      checks = forAllSystems (system: {
-        inherit (self.packages.${system}) hello;
-
-        # Additional tests, if applicable.
-        test =
-          with nixpkgsFor.${system};
-          stdenv.mkDerivation {
-            name = "hello-test-${version}";
-
-            buildInputs = [ hello ];
-
-            unpackPhase = "true";
-
-            buildPhase = ''
-              echo 'running some integration tests'
-              [[ $(hello) = 'Hello, world!' ]]
-            '';
-
-            installPhase = "mkdir -p $out";
-          };
-
-        # A VM test of the NixOS module.
-        vmTest =
-          with import (nixpkgs + "/nixos/lib/testing-python.nix") {
-            inherit system;
-          };
-
-          makeTest {
-            nodes = {
-              client = { ... }: {
-                imports = [ self.nixosModules.hello ];
-              };
-            };
-
-            testScript =
-              ''
-                start_all()
-                client.wait_for_unit("multi-user.target")
-                client.succeed("hello")
-              '';
-          };
-      });
-
-    };
+        
+        defaultPackage= deltabot;
+        packages= deltabot;
+      }
+      );
 }
