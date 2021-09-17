73d34b19a11d769aff508610d426a9674cf391c7
raghuram <raghurammailbox@gmail.com>
2021-09-17 06:26:56 +0000

    added tests

--- flake.nix
+++ flake.nix
@@ -6,41 +6,45 @@
     flake-utils.url = "github:numtide/flake-utils";
     mach-nix.url = "github:DavHau/mach-nix?ref=3.3.0";
     deltabot-scr = {
-      url = "https://files.pythonhosted.org/packages/c6/d7/90c24bbeef1e6b7e73dfe30e51e96699f5db05984497831ae2d87e93d9d4/deltabot-0.8.0.tar.gz"; 
-      flake = false;};
+      url = "https://files.pythonhosted.org/packages/c6/d7/90c24bbeef1e6b7e73dfe30e51e96699f5db05984497831ae2d87e93d9d4/deltabot-0.8.0.tar.gz";
+      flake = false;
     };
+  };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix,deltabot-scr }:
-  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  outputs = { self, nixpkgs, flake-utils, mach-nix, deltabot-scr }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         python = "python38";
         pkgs = nixpkgs.legacyPackages.${system};
         mach-nix-wrapper = import mach-nix { inherit pkgs python; };
-        requirements     = ''
+        requirements = ''
           setuptools
           attrs
           deltachat
-          '';
-        pythonBuild      = mach-nix-wrapper.mkPython { inherit requirements; };
-        deltabot         = mach-nix-wrapper.buildPythonPackage rec{
+          requests
+          pytest
+        '';
+        pythonBuild = mach-nix-wrapper.mkPython { inherit requirements; };
+        deltabot = mach-nix-wrapper.buildPythonPackage rec{
           version = "0.8.0";
           src = deltabot-scr;
-          requirementsExtra =''
-               setuptools
-               attrs
-               deltachat
-              '';
+          requirementsExtra = requirements;
+          tests = true;
+          checkPhase = ''
+            export HOME=/tmp
+            python3 -m pytest ./tests/
+          '';
         };
-      in {
+      in
+      {
         devShell = pkgs.mkShell {
-          buildInputs=[
+          buildInputs = [
             pythonBuild
           ];
         };
-        
-        defaultPackage= deltabot;
-        packages= deltabot;
-      }
-  );
 
+        defaultPackage = deltabot;
+        packages.deltabot = deltabot;
+      }
+    );
 }
